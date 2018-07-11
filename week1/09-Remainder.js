abeb9edea2cafe246c491d345d5fb854704d0a15
salar01 <39797683+salar01@users.noreply.github.com>
2018-07-11 22:46:22 +0200

    Add files via upload

---
+++ week1/09-Remainder.js
@@ -0,0 +1,13 @@
+var x = 7;
+x = x % 3;
+1
+
+console.log("the Modulus is the remainder of the one number by another");
+
+console.log("% is called the modulo operation");
+
+console.log("we divided 5 / 2 the result will be 2.5 that is division");
+
+console.log("we have x * y");
+4 * 3 //12
+ that is multiplication//
